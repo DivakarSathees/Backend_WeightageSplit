45035ef44f7544cf0bf85f904e246c40b80fc606
DivakarSathees <divakarsathees7@gmail.com>
2025-02-12 21:00:04 +0530

    id

--- server.js
+++ server.js
@@ -506,7 +506,7 @@ async function processZipFile(zipFilePath, evaluationTypes, projectType, fileNam
         //   console.error('File does not exist:', javaAppZipFilePath);
         //   return;
         // }
-        const fileId = new mongoose.Types.ObjectId('67acb9d3274df83ed2337b81');
+        const fileId = new mongoose.Types.ObjectId('67acbcdc057e3c6b4966373d');
         const downloadStream = gfsDefaultScaffolding.openDownloadStream(fileId);
         // create a folder with name defaultScaffoldings to store the zip file
         const defaultScaffoldingFolder = path.join(__dirname, 'defaultScaffoldings');
