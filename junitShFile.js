af6f3c9ec32b726da726614e26c21f6c445655be
DivakarSathees <divakarsathees7@gmail.com>
2025-02-12 20:38:54 +0530

    junitShfile Update

--- junitShFile.js
+++ junitShFile.js
@@ -12,9 +12,28 @@ async function junitShFile(extractionFolder, dynamicFolderName, subfolderContent
 
     // Create the script for junit.sh
     let scriptContent = `#!/bin/bash
-cp -r /home/coder/project/workspace/junit/test /home/coder/project/workspace/springapp/src/;
-cd /home/coder/project/workspace/springapp/;
-mvn clean test
+if [ ! -d "/home/coder/project/workspace/springapp" ]
+then
+    cp -r /home/coder/project/workspace/junit/springapp /home/coder/project/workspace/;
+fi
+rm -rf /home/coder/project/workspace/springapp/target/surefire-reports;
+src_directory="/home/coder/project/workspace/springapp/src"
+ 
+if [ -d "$src_directory" ]; then
+    cp -r /home/coder/project/workspace/junit/test "$src_directory"
+    cd /home/coder/project/workspace/springapp;
+    mvn -q test
+else 
+    echo "The 'src' folder does not exist in"
+fi
+ 
+cp -r /home/coder/project/workspace/junit/Report /home/coder/project/workspace;
+cd /home/coder/project/workspace/Report;
+source /usr/local/nvm/nvm.sh
+nvm use 14
+npm i
+node customReport.js
+rm -rf /home/coder/project/workspace/Report;
 `;
 
     // Create the `junit.sh` file with the generated content
